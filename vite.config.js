cd1c9b67c9ff16d75a1a36c0d0f978838c5446ee
huzan2 <huzan2@naver.com>
2024-07-25 16:43:39 +0000

    Setting API address

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// API 서버 주소를 환경 변수에서 가져옴
+const apiUrl = process.env.VITE_API_URL || 'http://localhost:8080';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080", // 실제 API 서버 주소
+        target: apiUrl, // 실제 API 서버 주소
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
