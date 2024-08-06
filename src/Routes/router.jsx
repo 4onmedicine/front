6b83e4d8c152f91ad289ae0d0b460be113f12d70
HANTAEDONG <htd0913@gmail.com>
2024-08-06 19:18:05 +0900

    fix: router 문제 해결

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -1,18 +1,15 @@
 import MainPage from "../pages/main/Mainpage";
-<<<<<<< HEAD
-=======
-import Instruction from "../components/detailsub/Instruction";
-import Caution from "../components/detailsub/Caution";
-import Interaction from "../components/detailsub/Interaction";
-import SideEffects from "../components/detailsub/SideEffects";
-import Storage from "../components/detailsub/Storage";
->>>>>>> parent of 579d0a0 (Chore: 로딩창 수정 및 에러 핸들링, 필요없는 코드 삭제)
 import ErrorPage from "../pages/ErrorPage";
 import DetailPage from "../pages/Detail/DetailPage";
 import Header from "../components/header/header";
 import PrescriptionPage from "../pages/Prescription/PrescriptionPage";
 import PrescriptionDetailPage from "../pages/PrescriptionDetail/PrescriptionDetailPage";
 import DetailTestPage from "../pages/DetailTestPage";
+import Instruction from "../components/detailsub/Instruction";
+import Caution from "../components/detailsub/Caution";
+import Interaction from "../components/detailsub/Interaction";
+import SideEffects from "../components/detailsub/SideEffects";
+import Storage from "../components/detailsub/Storage";
 
 const router = [
   {
